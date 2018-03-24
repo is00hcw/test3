20f916f0f69a79e2152fe2406c93ef47d96023cd
root <root@b88339be27de>
2018-03-24 17:36:12 +0800

    xx

---
+++ dao/src/main/java/com/pagoda/bgkj/repo/MemberRepository.java
@@ -0,0 +1,19 @@
+package com.pagoda.bgkj.repo;
+
+import com.pagoda.platform.jms.annotation.SqlTemplate;
+import com.pagoda.platform.jms.jpa.*;
+import com.pagoda.bgkj.api.dto.*;
+import com.pagoda.bgkj.domain.*;
+import com.pagoda.bgkj.repo.custom.*;
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.*;
+
+/**
+ * MemberJPA数据访问接口
+ *
+ * @author PagodaGenerator
+ * @generated
+ */
+public interface MemberRepository
+    extends MemberRepositoryCustom, PagodaJpaRepository<Member, Long> {}
