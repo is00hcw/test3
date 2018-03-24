d717d0afbf8339c7eedb34560f085fcac98a04f3
root <root@b88339be27de>
2018-03-24 17:40:47 +0800

    40

---
+++ service/src/main/java/com/pagoda/bgkj/service/config/ApplicationProperties.java
@@ -0,0 +1,21 @@
+package com.pagoda.bgkj.config;
+
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.spring.annotation.*;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * 从spring boot配置文件加载参数
+ *
+ * @author PagodaGenerator generated
+ */
+@Component
+@Data
+public class ApplicationProperties {
+
+  /** inject config for namespace application */
+  @ApolloConfig private Config config;
+}
